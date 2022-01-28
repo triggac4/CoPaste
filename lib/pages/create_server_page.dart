e15b32e73d4e95fd4f8a79c41b25587e3d07ab6f
kene3055 <koffokansi@gmail.com>
2022-01-28 10:11:33 +0100

    github copy

--- lib/pages/create_server_page.dart
+++ lib/pages/create_server_page.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
+import "package:permission_handler/permission_handler.dart";
 import 'package:co_paste/utils/http_server.dart';
 import "package:flutter/material.dart";
+import 'package:network_info_plus/network_info_plus.dart';
+import "package:http/http.dart" as http;
 
 class CreateServer extends StatefulWidget {
   static const String routeName = 'create-server';
@@ -11,24 +16,59 @@ class CreateServer extends StatefulWidget {
 }
 
 class _CreateServerState extends State<CreateServer> {
+  var httplocal = HttpLocalServer();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          height: 50,
-          width: 200,
-          child: TextButton.icon(
-              onPressed: () {
-                var httplocal = HttpLocalServer();
-                httplocal.createServer();
-              },
-              style: TextButton.styleFrom(
-                primary: Colors.white,
-                backgroundColor: Theme.of(context).colorScheme.primary,
-              ),
-              icon: const Icon(Icons.wifi),
-              label: const Text("Create A Server")),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 50,
+              width: 200,
+              child: TextButton.icon(
+                  onPressed: () async {
+                    await Permission.location.request();
+                    var networkInfo = NetworkInfo();
+                    var address = await networkInfo.getWifiIP();
+                    print(address);
+                    print(await NetworkInterface.list());
+                    if (httplocal.server == null) {
+                      httplocal.createServer();
+                    }
+
+                    http.post(Uri.parse("https://127.0.0.1:3000"),
+                        body: {'see': "saw", 'srr': "skk"});
+                  },
+                  style: TextButton.styleFrom(
+                    primary: Colors.white,
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                  ),
+                  icon: const Icon(Icons.wifi),
+                  label: const Text("Create A Server")),
+            ),
+            Container(
+              height: 50,
+              width: 200,
+              child: TextButton.icon(
+                  onPressed: () async {
+                    try {
+                      var response = await http
+                          .get(Uri.parse("http://192.168.3.221:3000"));
+                      print(response.body);
+                    } catch (e) {
+                      print(e);
+                    }
+                  },
+                  style: TextButton.styleFrom(
+                    primary: Colors.white,
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                  ),
+                  icon: const Icon(Icons.wifi),
+                  label: const Text("Create A Server")),
+            ),
+          ],
         ),
       ),
     );
--- lib/utils/http_server.dart
+++ lib/utils/http_server.dart
@@ -1,34 +1,35 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+
+import 'package:network_info_plus/network_info_plus.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class HttpLocalServer {
   HttpServer? server;
 
   createServer() async {
-    server = await HttpServer.bind(InternetAddress.loopbackIPv4, 7556);
+    //  await Permission.location.request();
+    //                   var networkInfo = NetworkInfo();
+    //                   var address = await networkInfo.getWifiIP();
+    //                   print(address);
+    server = await HttpServer.bind(InternetAddress.loopbackIPv4, 3000);
+    await handleRequests();
     print('address ${server?.address} port: ${server?.port}');
   }
 
   handleRequests() async {
-
-    var decoder=Utf8Decoder();
     if (server != null) {
       await for (var request in server!) {
         if (request.method == 'GET') {
           request.response
             ..write('some stuffs')
             ..close();
-        } 
-        if(request.method=="POST"){
-          request
-          ..transform()
-          ..
         }
-        else {
-          request.response
-            ..statusCode = 404
-            ..write('wrong')
-            ..close();
+        if (request.method == "POST") {
+          var value = await utf8.decoder.bind(request).join();
+          request.response.close();
+          print(value);
         }
       }
     }
